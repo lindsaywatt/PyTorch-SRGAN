c4c74a8d92dbe867e92afddc39b32ed6974aa7f8
Lindsay <lindsayawatt@gmail.com>
2018-11-07 15:46:50 +1000

    scale transform is depriciated

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ normalize = transforms.Normalize(mean = [0.485, 0.456, 0.406],
                                 std = [0.229, 0.224, 0.225])
 
 scale = transforms.Compose([transforms.ToPILImage(),
-                            transforms.Scale(opt.imageSize),
+                            transforms.Resize(opt.imageSize),
                             transforms.ToTensor(),
                             transforms.Normalize(mean = [0.485, 0.456, 0.406],
                                                 std = [0.229, 0.224, 0.225])
